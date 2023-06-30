0edb0da23657ee8b1bb901c451e005ef8ba37abe
obafemiolorungbon <obafedance@gmail.com>
2023-06-30 16:07:26 +0100

    feat: added new ports

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { ports } = require('./lib/ports')
-const { airports: airportsArray } = require('./lib/airports')
-const { airlines: airlinesArray } = require('./lib/airlines')
-const { shippingLines } = require('./lib/shippinglines')
+const { ports } = require('./lib/ports');
+const { airports: airportsArray } = require('./lib/airports');
+const { airlines: airlinesArray } = require('./lib/airlines');
+const { shippingLines } = require('./lib/shippinglines');
+const Terminals = require('./lib/terminals');
 
 const airports = airportsArray.reduce((acc, curr) => {
   const airport = {
@@ -9,74 +10,64 @@ const airports = airportsArray.reduce((acc, curr) => {
     city: curr['CITY']['AIRPORT'],
     name: `${curr['CITY']['AIRPORT']} (${curr['IATA CODE']})`,
     iata_code: curr['IATA CODE'],
-  }
-  return [...acc, { ...airport, type: 'city' }, { ...airport, type: 'port' }]
-}, [])
+  };
+  return [...acc, { ...airport, type: 'city' }, { ...airport, type: 'port' }];
+}, []);
 
-const airlines = airlinesArray.map((x) => x.airline)
-const shippinglines = shippingLines.map((x) => x.value)
+const airlines = airlinesArray.map((x) => x.airline);
+const shippinglines = shippingLines.map((x) => x.value);
 
 const shuffle = (array) => {
-  return array.sort(() => Math.random() - 0.5)
-}
+  return array.sort(() => Math.random() - 0.5);
+};
 
 const searchPortsByName = (name) => {
-  return ports.filter((port) =>
-    port.name.toLowerCase().includes(name.toLowerCase()),
-  )
-}
+  return ports.filter((port) => port.name.toLowerCase().includes(name.toLowerCase()));
+};
 
 const searchPortsByCity = (city) => {
-  return ports.filter((port) =>
-    (port.province || '').toLowerCase().includes(city.toLowerCase()),
-  )
-}
+  return ports.filter((port) => (port.province || '').toLowerCase().includes(city.toLowerCase()));
+};
 
 const searchPortsByCountry = (country) => {
-  return ports.filter((port) =>
-    port.country.toLowerCase().includes(country.toLowerCase()),
-  )
-}
+  return ports.filter((port) => port.country.toLowerCase().includes(country.toLowerCase()));
+};
 
 const searchByPorts = (searchString) => {
   return shuffle([
     ...searchPortsByName(searchString),
     ...searchPortsByCity(searchString),
     ...searchPortsByCountry(searchString),
-  ])
-}
+  ]);
+};
 
 // airports
 
 const searchAirPortsByName = (name) => {
-  return airports.filter((airline) =>
-    airline.name.toLowerCase().includes(name.toLowerCase()),
-  )
-}
+  return airports.filter((airline) => airline.name.toLowerCase().includes(name.toLowerCase()));
+};
 
 const searchAirPortsByCountry = (country) => {
   return airports.filter((airline) =>
     airline.country.toLowerCase().includes(country.toLowerCase()),
-  )
-}
+  );
+};
 
 const searchAirPortsByIATACode = (code) => {
-  return airports.filter((airline) =>
-    airline.iata_code.toLowerCase().includes(code.toLowerCase()),
-  )
-}
+  return airports.filter((airline) => airline.iata_code.toLowerCase().includes(code.toLowerCase()));
+};
 
 const searchByAirports = (string) => {
   return shuffle([
     ...searchAirPortsByCountry(string),
     ...searchAirPortsByName(string),
     ...searchAirPortsByIATACode(string),
-  ])
-}
+  ]);
+};
 
 const searchAllPorts = (value) => {
-  return shuffle([...searchByAirports(value), ...searchByPorts(value)])
-}
+  return shuffle([...searchByAirports(value), ...searchByPorts(value)]);
+};
 
 // console.log('sds', searchAirPortsByIATACode('AMA'))
 // console.log('sds', searchByAirports('AMA'))
@@ -94,4 +85,4 @@ module.exports = {
   searchAllPorts,
   AIRLINES: airlines,
   SHIPPINGLINES: shippinglines,
-}
+};
